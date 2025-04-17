8731d56ac405bd3f5ab3342b55c7bb44348ebaf7
shailesj121 <shaileshbisht9876@gmail.com>
2025-04-17 15:49:41 +0530

    add inport fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Footer from "./components/footer";
 import Header from "./components/header"
 import { Routes, Route, useLocation  } from 'react-router-dom';
-import Home from "./pages/Home";
-import NotFound from "./pages/Nopage";
-import Vision from "./pages/vision";
+import Home from "./pages/Home.jsx";
+import NotFound from "./pages/Nopage.jsx";
+import Vision from "./pages/vision.jsx";
 import Insights from "./pages/insights.jsx";
-import Contact from "./pages/Contact";
+import Contact from "./pages/Contact.jsx";
 import { AnimatePresence } from 'framer-motion';
 import { useEffect } from "react";
 
