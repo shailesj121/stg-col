c06e88917f778ae27b05543b36f6916a7b40df4b
shailesj121 <shaileshbisht9876@gmail.com>
2025-04-22 15:45:05 +0530

    contact class chages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact(){
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }} className="main">
         <section className="mt-5 pt-1">
-            <Container  className='mt--5 pt-3 p-0'>
+            <Container  className='mt-sm-5 pt-3 p-0'>
                     <Bubble/>
                 <Row className='mt-0 mt-md-5 pt-5   position-relative p-2'>
                     <div className="col-0 col-sm-6  my-3 my-sm-0">
